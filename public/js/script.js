6f30bfb283d9bebea28b3ec210faae7df01f66ef
Melhorias-Continuas <melhoras.continuasdorel@gmail.com>
2025-02-17 20:24:42 -0300

    Adicionando POST no formulário

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,25 @@
-let qtd = 0;
-
 const submitButton = document.querySelector('.submitButton');
 const form = document.querySelector('form');
 const table = document.querySelector('table');
+const url = 'http://localhost:3000/items'
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro na requisição: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        data.forEach(p => {
+            constTr(p.name, p.quantity, p.id )
+        })
+      })
+      .catch(error => console.error('Erro ao buscar os itens:', error));
+  });
+  
+  
 
 submitButton.addEventListener('click', (ev) => {
     ev.preventDefault();
@@ -10,27 +27,12 @@ submitButton.addEventListener('click', (ev) => {
     const name = document.querySelector('#name');
     const quantity = document.querySelector('#quantity');
 
-    const tr = document.createElement('tr');
-
-    const prod = document.createElement('td');
-    prod.innerText = name.value;
-    prod.style.textAlign = 'left';
-
-    const qty = document.createElement('td');
-    qty.innerText = quantity.value;
-
-    qtd += 1;
-
-    const edit = document.createElement('td');
-    edit.appendChild(editBtn(qtd));
-    edit.appendChild(deleteBtn(qtd));
-
-
-    tr.appendChild(prod);
-    tr.appendChild(qty);
-    tr.appendChild(edit);
-
-    table.appendChild(tr);
+    // Chama a função passando os valores dos inputs
+    createProduct(name.value, Number(quantity.value))
+      .then(status => {
+        console.log("Status da requisição:", status);
+      })
+      .catch(error => console.error("Erro ao criar o produto:", error));
 
     // Limpar os campos do formulário após adicionar a linha
     name.value = '';
@@ -59,4 +61,47 @@ function deleteBtn(qtd) {
     del.title = 'Excluir';
 
     return del;
+}
+
+function constTr (name, quantity, id) {
+
+    const tr = document.createElement('tr');
+    tr.classList.add('product-tr',id)
+
+    const prod = document.createElement('td');
+    prod.innerText = name;
+    prod.style.textAlign = 'left';
+
+    const qty = document.createElement('td');
+    qty.innerText = quantity;
+
+    const edit = document.createElement('td');
+    edit.appendChild(editBtn(id));
+    edit.appendChild(deleteBtn(id));
+
+
+    tr.appendChild(prod);
+    tr.appendChild(qty);
+    tr.appendChild(edit);
+
+    table.appendChild(tr);
+
+}
+
+async function createProduct(name_, quantity_){
+
+  const data = {name: name_, quantity: quantity_}
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  const product = await response.json();
+  console.log(product);
+  constTr(product.name, product.quantity, product.id);
+  return response.status;
 }
